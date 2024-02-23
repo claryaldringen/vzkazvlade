bd11d90d8237fa6152711e55d61a3ddd67596cba
Martin Zadrazil <martin.zadrazil@ibm.com>
2024-02-23 16:58:29 +0100

    feat: add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,21 @@ const App = ({ Component, pageProps }: AppProps) => {
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1872562528039384"
                     crossOrigin="anonymous"
                 />
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-QT9WNYYE4D"
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-QT9WNYYE4D');
+            `,
+                    }}
+                />
                 <link rel="icon" href="/favicon.jpeg" />
                 <title>Vzkaz fialové vládě</title>
             </Head>
